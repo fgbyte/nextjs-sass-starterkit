3b9b5cef3a13f77584c6eb17a4ac9ba6e843cc05
fgbyte <fer.garciabayon@gmail.com>
2025-01-29 14:55:46 -0500

    add sign-in and sign-up pages with Clerk integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
 import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
@@ -30,8 +29,6 @@ export default function RootLayout({
 				<body
 					className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 				>
-					<Header />
-
 					{children}
 				</body>
 			</html>
